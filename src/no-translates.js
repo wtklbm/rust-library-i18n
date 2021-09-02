c7c1ecdaf7dc5a3f32f0fdcdc393f2d45d25ce16
wtklbm <wtklbm@gmail.com>
2021-09-02 17:37:13 +0800

    ✨ feat: 添加 `json` 文件

---
+++ src/no-translates.js
@@ -0,0 +1,15 @@
+// If you think some words should not be translated, please add them here.
+//
+// NOTE: Adding words requires careful consideration, the added words will be applied to all source files.
+
+const noTranslates = [
+    // INFO: Not case-sensitive.
+    'crate',
+    'RESTful',
+    'promise',
+    'futures',
+
+    // Please add here ...
+];
+
+module.exports = noTranslates;
---
+++ src/terminologies.js
@@ -0,0 +1,27 @@
+// This file is used to save computer terminology.
+// If you think some of the terms are incorrectly translated, then please add the translation you think is correct here.
+//
+// NOTE: Adding words requires careful consideration, the added words will be applied to all source files.
+
+const terminologies = {
+    // `double free` is computer terminology
+    // INFO: Not case-sensitive.
+    'double free': {
+        // Attribute name is `ISO-639-1` code.
+        // see：https://cloud.google.com/translate/docs/languages
+        'zh-CN': '双重释放',
+        'zh-TW': '雙重釋放',
+        // Please add here ...
+    },
+
+    // `race condition` is computer terminology
+    'race condition': {
+        'zh-CN': '竞态条件',
+        'zh-TW': '競態條件',
+        // Please add here ...
+    },
+
+    // add more ...
+};
+
+module.exports = terminologies;
